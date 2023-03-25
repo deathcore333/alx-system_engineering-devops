e87784ee10b3e39a10940ec50eb3544f89109792
deathcore333 <amossmwangii@gmail.com>
2023-03-25 06:26:35 +0300

    corrected the style requirement using puppet-lint

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,4 @@
-# install puppet-lint. version must be 2.5.0
-exec { 'puppet-lint':
-	command => '/bin/gem install puppet-lint -v 2.5.0'
+# install flask version must be 2.1.0
+exec { 'flask':
+    command => '/usr/bin/pip3 install flask==2.1.0'
 }

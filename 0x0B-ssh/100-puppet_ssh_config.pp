75da84a184124b3d40b430662bc5e70bbadb487a
deathcore333 <amossmwangii@gmail.com>
2023-04-04 01:11:23 +0300

    using  puppet to control server operations

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+# Use puppet to change config-file and remove authorization
+
+file_line { 'Declare identity file':
+  path    => '/etc/ssh/ssh_config',
+  line    => '    IdentityFile ~/.ssh/school',
+  replace => true,
+}
+
+file_line { 'Turn off passwd auth':
+  path    => '/etc/ssh/ssh_config',
+  line    => '    PasswordAuthentication no',
+  replace => true,
+}
+

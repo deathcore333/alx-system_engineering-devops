753deeebe57c794e9b7a709a8a8acabfd700d005
deathcore333 <amossmwangii@gmail.com>
2023-04-11 14:34:05 +0300

    another syntax error

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,8 @@
 # kills a process named killmenow
 exec { 'killmenow'
-    command  => '/usr/bin/pkill killmenow',
-    provider => 'shell',
-    returns  => [0, 1],
+    command   => '/usr/bin/pkill killmenow',
+    provider  => 'shell',
+    returns   => [0, 1],
+    tries     => 3,
+    try_sleep => 5,
 }

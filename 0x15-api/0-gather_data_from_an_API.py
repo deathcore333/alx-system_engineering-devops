9c9914699c5d1255b48eda11805f4b0f318ca1bd
deathcore333 <amossmwangii@gmail.com>
2023-04-26 22:56:44 +0300

    corrected style

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-''' fetches data from a given employee ID 
+''' fetches data from a given employee ID
     Return: info about her TODO list progress
 '''
 
@@ -11,11 +11,11 @@ if __name__ == "__main__":
     if len(argv) > 1:
         userID = int(argv[1])
         url = "https://jsonplaceholder.typicode.com/users/"
-        r = requests.get("{}/{}".format(url, userId))
+        r = requests.get("{}/{}".format(url, userID))
         data = r.json()
         name = r.json().get("name")
         if name is not None:
-            userTodos = requests.get("{}{}/todos".format(url, userId))
+            userTodos = requests.get("{}{}/todos".format(url, userID))
             userTodos = userTodos.json()
             taskNumber = len(userTodos)
             completedTasks = []
@@ -24,7 +24,7 @@ if __name__ == "__main__":
                     completedTasks.append(task)
             taskCompletedNumber = len(completedTasks)
             print("Employee {} is done with tasks({}/{}):"
-                    .format(name, taskCompletedNumber, taskNumber))
+                  .format(name, taskCompletedNumber, taskNumber))
             for task in completedTasks:
                 title = task.get("title")
                 print("\t {}".format(title))

55dddbc9edf7f1be9282e3f046e0928765a0792c
deathcore333 <amossmwangii@gmail.com>
2023-04-26 22:53:00 +0300

    new code

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+''' fetches data from a given employee ID 
+    Return: info about her TODO list progress
+'''
+
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    if len(argv) > 1:
+        userID = int(argv[1])
+        url = "https://jsonplaceholder.typicode.com/users/"
+        r = requests.get("{}/{}".format(url, userId))
+        data = r.json()
+        name = r.json().get("name")
+        if name is not None:
+            userTodos = requests.get("{}{}/todos".format(url, userId))
+            userTodos = userTodos.json()
+            taskNumber = len(userTodos)
+            completedTasks = []
+            for task in userTodos:
+                if task.get("completed") is True:
+                    completedTasks.append(task)
+            taskCompletedNumber = len(completedTasks)
+            print("Employee {} is done with tasks({}/{}):"
+                    .format(name, taskCompletedNumber, taskNumber))
+            for task in completedTasks:
+                title = task.get("title")
+                print("\t {}".format(title))
---
+++ 0x15-api/README.md
@@ -0,0 +1,14 @@
+API - Application Programming Interface
+An API is a program containing a set of routines, protocols and tools for building application.
+APIs can be provided by application providers such as twitter or github for different purposes, such as scrapping some form of data or building a bot user.
+The API specifies the software component interaction
+REST API Representation State Transfer
+This type of API allows two computers/servers to communicate without interacting with the user  through the HTTP technology.
+The steps to creating a REST API request include:
+1. End point URL
+2. Specify the HTTP method such as:
+	a. GET for read
+	b. POST for create
+	c. PUT or PATCH for update
+	d. DELETE for clear
+			D
